023a48c265e1347b5326303cb2651eb4c2dfedd1
Tyler Williams <twilliams@jumptech.com>
2016-04-07 11:40:46 -0500

    Dramatic efficiency improvements to ReflectionUtils

--- src/main/java/com/tyler/sqlplus/utility/ReflectionUtils.java
+++ src/main/java/com/tyler/sqlplus/utility/ReflectionUtils.java
@@ -1,63 +1,98 @@
 package com.tyler.sqlplus.utility;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
 
 public final class ReflectionUtils {
 
+	private static final Map<Field, Function<Object, Object>> FIELD_GET = new HashMap<>();
+	private static final Map<Field, BiConsumer<Object, Object>> FIELD_SET = new HashMap<>();
+	private static final Map<Field, Class<?>> FIELD_GENERIC = new HashMap<>();
+	
 	private ReflectionUtils() {}
 
 	public static Class<?> getGenericType(Field f) {
-		return (Class<?>) ((ParameterizedType)f.getGenericType()).getActualTypeArguments()[0];
+		if (FIELD_GENERIC.containsKey(f)) {
+			return FIELD_GENERIC.get(f);
+		}
+		else {
+			Class<?> genericType = (Class<?>) ((ParameterizedType)f.getGenericType()).getActualTypeArguments()[0];
+			FIELD_GENERIC.put(f, genericType);
+			return genericType;
+		}
 	}
 	
-	public static Object get(String field, Object o) throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException {
-		return get(o.getClass().getDeclaredField(field), o);
+	public static Object get(String fieldName, Object o) throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException {
+		return get(o.getClass().getDeclaredField(fieldName), o);
 	}
-
-	public static Object get(Field field, Object o) throws IllegalArgumentException, IllegalAccessException {
-		try {
-			String fieldName = field.getName();
-			fieldName = "set" + String.valueOf(fieldName.charAt(0)).toUpperCase() + fieldName.substring(1).toLowerCase();
-			try {
-				return o.getClass().getDeclaredMethod("get" + fieldName).invoke(o);
-			} catch (NoSuchMethodException e1) {
-				return o.getClass().getDeclaredMethod("is" + fieldName).invoke(o);
-			}
-		} catch (Exception e) {
-			field.setAccessible(true);
-			return field.get(o);
+	
+	public static Object get(Field field, Object o) throws IllegalArgumentException, IllegalAccessException, SecurityException {
+		Function<Object, Object> getValue = null;
+		if (FIELD_GET.containsKey(field)) {
+			getValue = FIELD_GET.get(field);
 		}
+		else {
+			try {
+				try {
+					Method getter = o.getClass().getDeclaredMethod("get" + field);
+					getValue = (obj) -> { try { return getter.invoke(obj); } catch (Exception e) { throw new RuntimeException(e); } }; 
+				} catch (NoSuchMethodException e1) {
+					Method getter = o.getClass().getDeclaredMethod("is" + field);
+					getValue = (obj) -> { try { return getter.invoke(obj); } catch (Exception e) { throw new RuntimeException(e); } };
+				}
+			}
+			catch (Exception e) {
+				getValue = (obj) -> {
+					field.setAccessible(true);
+					try {
+						return field.get(obj);
+					} catch (Exception e1) {
+						throw new RuntimeException(e1);
+					}
+				};
+			}
+			FIELD_GET.put(field, getValue);
+		}
+		
+		return getValue.apply(o);
 	}
 
 	public static void set(String member, Object o, Object value) throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException {
 		set(o.getClass().getDeclaredField(member), o, value);
 	}
 
-	public static void set(Field field, Object obj, Object value) throws IllegalArgumentException, IllegalAccessException {
-		try {
-			String fieldName = field.getName();
-			String methodName = "set" + String.valueOf(fieldName.charAt(0)).toUpperCase() + fieldName.substring(1).toLowerCase();
-			obj.getClass().getDeclaredMethod(methodName, field.getType()).invoke(obj, value);
-		} catch (NoSuchMethodException | InvocationTargetException e) {
-			field.setAccessible(true);
-			field.set(obj, value);
+	public static void set(Field field, Object o, Object value) throws IllegalArgumentException, IllegalAccessException {
+		BiConsumer<Object, Object> setValue = null;
+		
+		if (FIELD_SET.containsKey(field)) {
+			setValue = FIELD_SET.get(field);
 		}
-	}
-
-	/**
-	 * Takes a basic map of string -> string and maps it to a POJO instance.
-	 * The keys in the map are expected to be the pojo field names and the values are the values for the fields
-	 */
-	public static <T> T toPOJO(Map<String, Object> map, Class<T> pojo) throws InstantiationException, IllegalAccessException, NoSuchFieldException {
-		T instance = pojo.newInstance();
-		for (Map.Entry<String, Object> e : map.entrySet()) {
-			Field f = pojo.getDeclaredField(e.getKey());
-			f.set(instance, e.getValue());
+		else {
+			try {
+				String fieldName = field.getName();
+				fieldName = String.valueOf(fieldName.charAt(0)).toUpperCase() + fieldName.substring(1);
+				Method setter = o.getClass().getDeclaredMethod("set" + fieldName, value.getClass());
+				setValue = (obj, val) -> { try { setter.invoke(obj, val); } catch (Exception e) { throw new RuntimeException(e); } }; 
+			}
+			catch (Exception e) {
+				setValue = (obj, val) -> {
+					field.setAccessible(true);
+					try {
+						field.set(obj, val);
+					} catch (Exception e1) {
+						throw new RuntimeException(e1);
+					}
+				};
+			}
+			FIELD_SET.put(field, setValue);
 		}
-		return instance;
+		
+		setValue.accept(o, value);
 	}
 	
 }
