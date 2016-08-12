8e66da5ca9944840c47c8355a53c2a71493d7640
Tyler Williams <twilliams@jumptech.com>
2016-08-12 16:02:48 -0500

    Add configuration object

---
+++ src/main/java/com/tyler/sqlplus/Configuration.java
@@ -0,0 +1,46 @@
+package com.tyler.sqlplus;
+
+public class Configuration {
+
+	private String url;
+	private String username;
+	private String password;
+	private boolean convertCamelCaseToUnderscore = false;
+	
+	public String getUrl() {
+		return url;
+	}
+	
+	public Configuration setUrl(String url) {
+		this.url = url;
+		return this;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public Configuration setUsername(String username) {
+		this.username = username;
+		return this;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public Configuration setPassword(String password) {
+		this.password = password;
+		return this;
+	}
+
+	public boolean isConvertCamelCaseToUnderscore() {
+		return convertCamelCaseToUnderscore;
+	}
+
+	public Configuration setConvertCamelCaseToUnderscore(boolean convertCamelCaseToUnderscore) {
+		this.convertCamelCaseToUnderscore = convertCamelCaseToUnderscore;
+		return this;
+	}
+	
+}
