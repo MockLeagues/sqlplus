7331f03b7b4e642eb11830eae0c25b6a29054e01
Tyler Williams <twilliams@jumptech.com>
2016-08-13 14:28:51 -0500

    Use method filter when determining to intercept method for lazy loading

--- src/main/java/com/tyler/sqlplus/proxy/EntityProxy.java
+++ src/main/java/com/tyler/sqlplus/proxy/EntityProxy.java
@@ -1,7 +1,6 @@
 package com.tyler.sqlplus.proxy;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -10,7 +9,6 @@ import com.tyler.sqlplus.annotation.LoadQuery;
 import com.tyler.sqlplus.exception.LazyLoadException;
 import com.tyler.sqlplus.utility.Fields;
 
-import javassist.util.proxy.MethodHandler;
 import javassist.util.proxy.Proxy;
 import javassist.util.proxy.ProxyFactory;
 
@@ -27,61 +25,63 @@ public class EntityProxy {
 		
 		ProxyFactory factory = new ProxyFactory();
 		factory.setSuperclass(type);
+		
+		// We intercept a method call to lazy-load only if both are true:
+		// 1) We haven't loaded it yet (duh) AND
+		// 2) Either the method has a @LoadQuery annotation or has a 'get' prefix
+		final Set<String> gettersLoaded = new HashSet<>();
+		factory.setFilter(invokedMethod -> {
+			String methodName = invokedMethod.getName();
+			return !gettersLoaded.contains(methodName) &&
+					(methodName.startsWith("get") || invokedMethod.isAnnotationPresent(LoadQuery.class));
+		});
+		
 		T proxy = (T) factory.createClass().newInstance();
 		
-		((Proxy)proxy).setHandler(new MethodHandler() {
+		((Proxy)proxy).setHandler((self, invokedMethod, proceed, args) -> {
 			
-			private Set<String> gettersAlreadyLoaded = new HashSet<>();
+			// 2 critical pieces of data we need to find in order to lazy-load
+			LoadQuery loadQueryAnnot = null;
+			Field loadField = null;
 			
-			@Override
-			public Object invoke(Object self, Method invokedMethod, Method proceed, Object[] args) throws Throwable {
-				
-				String methodName = invokedMethod.getName();
-				boolean hasGetPrefix = methodName.startsWith("get");
-				boolean isMethodAnnotated = invokedMethod.isAnnotationPresent(LoadQuery.class);
-				boolean isAlreadyLoaded = gettersAlreadyLoaded.contains(methodName);
-				
-				if (!isAlreadyLoaded && (hasGetPrefix || isMethodAnnotated)) {
-
-					LoadQuery loadQueryAnnot = null;
-					Field loadField = null;
-					
-					if (isMethodAnnotated) {
-						loadQueryAnnot = invokedMethod.getDeclaredAnnotation(LoadQuery.class);
-						if (!loadQueryAnnot.field().isEmpty()) {
-							loadField = type.getDeclaredField(loadQueryAnnot.field());
-						}
-						else if (hasGetPrefix) {
-							String loadFieldName = Fields.extractFieldName(methodName);
-							try {
-								loadField = type.getDeclaredField(loadFieldName);
-							}
-							catch (NoSuchFieldException ex) {
-								throw new LazyLoadException(
-									"Inferred lazy-load field '" + loadFieldName + "' not found when executing method " + invokedMethod);
-							}
-						}
-						else {
-							throw new LazyLoadException("Could not determine field to lazy-load to");
-						}
-					}
-					else if (hasGetPrefix) {
-						String loadFieldName = Fields.extractFieldName(methodName);
+			String methodName = invokedMethod.getName();
+			boolean hasGetPrefix = methodName.startsWith("get");
+			boolean isMethodAnnotated = invokedMethod.isAnnotationPresent(LoadQuery.class);
+			
+			if (isMethodAnnotated) {
+				loadQueryAnnot = invokedMethod.getDeclaredAnnotation(LoadQuery.class);
+				if (!loadQueryAnnot.field().isEmpty()) {
+					loadField = type.getDeclaredField(loadQueryAnnot.field());
+				}
+				else if (hasGetPrefix) {
+					String loadFieldName = Fields.extractFieldName(methodName);
+					try {
 						loadField = type.getDeclaredField(loadFieldName);
-						if (loadField.isAnnotationPresent(LoadQuery.class)) {
-							loadQueryAnnot = loadField.getDeclaredAnnotation(LoadQuery.class);
-						}
 					}
-					
-					if (loadField != null && loadQueryAnnot != null) {
-						Object loadedResult = LazyLoader.load(self, loadField, loadQueryAnnot.value(), session);
-						Fields.set(loadField, self, loadedResult);
-						gettersAlreadyLoaded.add(methodName);
+					catch (NoSuchFieldException ex) {
+						throw new LazyLoadException(
+							"Inferred lazy-load field '" + loadFieldName + "' not found when executing method " + invokedMethod);
 					}
 				}
-				
-				return proceed.invoke(self, args);
+				else {
+					throw new LazyLoadException("Could not determine field to lazy-load to");
+				}
 			}
+			else if (hasGetPrefix) {
+				String loadFieldName = Fields.extractFieldName(methodName);
+				loadField = type.getDeclaredField(loadFieldName);
+				if (loadField.isAnnotationPresent(LoadQuery.class)) {
+					loadQueryAnnot = loadField.getDeclaredAnnotation(LoadQuery.class);
+				}
+			}
+			
+			if (loadField != null && loadQueryAnnot != null) {
+				Object loadedResult = LazyLoader.load(self, loadField, loadQueryAnnot.value(), session);
+				Fields.set(loadField, self, loadedResult);
+				gettersLoaded.add(methodName);
+			}
+			
+			return proceed.invoke(self, args);
 		});
 		
 		return proxy;
